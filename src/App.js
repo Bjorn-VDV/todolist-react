d063378f2e60b6a017f12c068ae3c6f3b48cadb5
bjorn-vdv <bjorn_vandevelde@hotmail.com>
2022-05-18 11:37:46 +0200

    I am not a robot checkbox. For fun.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App()
 {
   const TodoNameRef = useRef();
   const [todos, setTodos] = useState([]);
+  const [isNotRobot, setIsNotRobot] = useState(false);
 
 
   // Saving the code in a local storage, so refreshing does not remove the entire list
@@ -40,18 +41,25 @@ function App()
   // Set name to the nameref's CURRENT VALUE. Not adding ".current.value" this breaks the entire code.
   // If the name is empty, we don't add a thing.
   // Otherwise, we set out new todolist through a sort of foreach loop.
-      // prevTodos will take all the todos we have, and we add a new one to it with values:
-          // ID: Created by RNG. Name: Name reference. Completes: Defaulted to false.
+  // prevTodos will take all the todos we have, and we add a new one to it with values:
+  // ID: Created by RNG. Name: Name reference. Completes: Defaulted to false.
   // We then set the nameref back to null.
   let HandleAddTodo = () =>
   {
-    const name = TodoNameRef.current.value
-    if (name === "") return
-    setTodos(prevTodos =>
+    if (!isNotRobot)
     {
-      return [...prevTodos, { id: uuidv4(), name: name, complete: false }]
-    })
-    TodoNameRef.current.value = null
+      alert("Please make sure you are not a robot.")
+    }
+    else
+    {
+      const name = TodoNameRef.current.value
+      if (name === "") return
+      setTodos(prevTodos =>
+      {
+        return [...prevTodos, { id: uuidv4(), name: name, complete: false }]
+      })
+      TodoNameRef.current.value = null
+    }
   }
 
   // Create a new array of todo objects by only saving those where .complete is false.
@@ -61,13 +69,19 @@ function App()
     setTodos(newTodos)
   }
 
+  let handleIsRobot = () =>
+  {
+    setIsNotRobot(current => !current);
+  }
+
   // - We throw the function toggletodo to the TodoList. Inside TodoList, this function will
-      // be thrown towards Todo, where it will be used for the checkbox.
+  // be thrown towards Todo, where it will be used for the checkbox.
   // - We throw the individual todo-array into the TodoList where it will convert them into a list.
   // - We use TodoNameRef to store the input text as a reference which will be set to the name of the todo.
   // - Two buttons, each have their own function above.
   // - We filter the todos by removing those that are complete, and then check how many objects are in there.
-      // This will show how many todo's are left to do.
+  // This will show how many todo's are left to do.
+  // Adding a "not a robot" checkbox because why not.
   return (
     <>
       <TodoList todos={todos} toggleTodo={toggleTodo} />
@@ -75,6 +89,7 @@ function App()
       <button onClick={HandleAddTodo}>Add Todo</button>
       <button onClick={ClearCompleted}>Clear completed todos</button>
       <div>{todos.filter(todos => !todos.complete).length} todos left</div>
+      <label><input type="checkbox" value={isNotRobot} onChange={handleIsRobot}></input> I am not a Robot</label>
     </>
   )
 }
